9e80cf6c48eda76d8a98070adc305d2eda6ac83d
Taras-SH13 <13tsh2019@gmail.com>
2020-08-01 11:49:46 +0300

    Add delete method in OrderDetailRepositoryImpl.

--- src/main/java/org/shop/Main.java
+++ src/main/java/org/shop/Main.java
@@ -14,6 +14,10 @@ public class Main {
         OrderDetailEntity orderDetail2=new OrderDetailEntity(2,"Mouse",3);
         OrderDetailEntity orderDetail3=new OrderDetailEntity(3,"RAM",42);
         OrderDetailEntity orderDetail4=new OrderDetailEntity(4,"Memory",3);
+        OrderDetailEntity orderDetail5=new OrderDetailEntity(5,"Matheboard",46);
+        OrderDetailEntity orderDetail6=new OrderDetailEntity(6,"Mouse",5);
+        OrderDetailEntity orderDetail7=new OrderDetailEntity(7,"RAM Toshiba",46);
+        OrderDetailEntity orderDetail8=new OrderDetailEntity(8,"Memory Samsung",36);
 
         List<OrderDetailEntity> orderDetailEntities=new ArrayList<>();
 
@@ -21,20 +25,26 @@ public class Main {
         orderDetailEntities.add(orderDetail2);
 
         List<OrderDetailEntity> orderDetailEntities2=new ArrayList<>();
-        orderDetailEntities.add(orderDetail3);
-        orderDetailEntities.add(orderDetail4);
+        orderDetailEntities2.add(orderDetail3);
+        orderDetailEntities2.add(orderDetail4);
 
         OrderEntity orderEntity1=new OrderEntity(1,"Computer Parts","Ivanov",orderDetailEntities);
         OrderEntity orderEntity2=new OrderEntity(3,"Parts","Petrov",orderDetailEntities2);
 
-      //  OrderDetailRepositoryImpl orderDetailRepository=new OrderDetailRepositoryImpl();
+        List<OrderDetailEntity> orderDetailEntities3=new ArrayList<>();
+        orderDetailEntities3.add(orderDetail5);
+        orderDetailEntities3.add(orderDetail6);
+        orderDetailEntities3.add(orderDetail7);
+        OrderEntity orderEntity3=new OrderEntity(4,"Spear Parts","LTD",orderDetailEntities3);
 
-//        System.out.println(orderDetailRepository.findAllDetailInCurrentOrder(2));
-//        System.out.println(new OrderRepositoryImpl().findAllOrdersInDB());
-//        System.out.println(new OrderDetailRepositoryImpl().findDetailByIDinCurrentOrder(3,2));
-        //System.out.println(new OrderRepositoryImpl().findOrderByID(2));
-        OrderRepositoryImpl saveOrder=new OrderRepositoryImpl();
-        saveOrder.saveOrderInDB(orderEntity2);
+
+
+       OrderDetailRepositoryImpl orderDetailRepository=new OrderDetailRepositoryImpl();
+//        OrderRepositoryImpl orderRepository=new OrderRepositoryImpl();
+//        orderRepository.saveOrderInDB(orderEntity3);
+//        orderDetailRepository.saveOrderDetailInDB(orderEntity3);
+        orderDetailRepository.deleteOrderDetailFromDB(orderEntity3,1);
+
     }
 
 }
